bee562b2d574ee6492911b05d6e208827256fe05
Ignacio Ramirez Paulino <nacho@fing.edu.uy>
2022-02-16 02:10:41 -0300

    goot

--- src/povhack.c
+++ src/povhack.c
@@ -91,6 +91,69 @@ int main ( int argc, char * argv[] ) {
 
 //------------------------------------------------------------
 
+static void put_floor(int x, int y, FILE*outf) {
+  fprintf( outf, "object { Floor ");
+  fprintf( outf, " translate < %2d, %2d,  0 > ", x, y);
+  fprintf( outf, " texture {FloorTexture} } \n");
+}
+
+
+static void put_corner(int x, int y, int angle, FILE* outf) {
+  put_floor(x,y,outf);
+  fprintf( outf, "object { Corner rotate %d*z ", angle); 
+  fprintf( outf, " translate < %2d, %2d,  0 > ", x, y);
+  fprintf( outf, " texture {WallTexture} } \n");
+}
+
+
+static void put_wall(int x, int y, int angle, FILE* outf) {
+  put_floor(x,y,outf);
+  fprintf( outf, "object { Wall rotate %d*z ", angle); 
+  fprintf( outf, " translate < %2d, %2d,  0 > ", x, y);
+  fprintf( outf, " texture {WallTexture} } \n");
+}
+
+static void put_door(int x, int y, int angle, FILE* outf) {
+  put_floor(x,y,outf);
+  fprintf( outf, "object { Door rotate %d*z ", angle); 
+  fprintf( outf, " translate < %2d, %2d,  0 > }\n", x, y);
+}
+
+static void put_ladder_up(int x, int y, FILE* outf) {
+  put_floor(x,y,outf);
+  fprintf( outf, "object { Ladder ");
+  fprintf( outf, " translate < %2d, %2d,  0 > }\n", x, y);
+}
+
+static void put_ladder_down(int x, int y, FILE* outf) {
+  // no floor
+  fprintf( outf, "object { Ladder ");
+  fprintf( outf, " translate < %2d, %2d,  0 > }\n", x, y);
+}
+
+static void put_altar(int x, int y, FILE* outf) {
+  put_floor(x,y,outf);
+  fprintf( outf, "object { Trap ");
+  fprintf( outf, " translate < %2d, %2d,  0 > }\n", x, y);
+}
+
+static void put_fountain(int x, int y, FILE* outf) {
+  put_floor(x,y,outf);
+  fprintf( outf, "object { Fountain ");
+  fprintf( outf, " translate < %2d, %2d,  0 > }\n", x, y);
+}
+
+static void put_water(int x, int y, FILE* outf) {
+  put_floor(x,y,outf);
+  fprintf( outf, "object { Water ");
+  fprintf( outf, " translate < %2d, %2d,  0 > }\n", x, y);
+}
+
+static void put_trap(int x, int y, FILE* outf) {
+  fprintf( outf, "object { Trap ");
+  fprintf( outf, " translate < %2d, %2d,  0 > }\n", x, y);
+}
+
 void map_to_pov(terminal_t* term,
 		int frame,
 		double time,
@@ -101,92 +164,190 @@ void map_to_pov(terminal_t* term,
   fprintf ( outf, "#declare GameFrame = %d;\n", frame );
   fprintf ( outf, "#declare GameSubFrame = %f;\n", time );
   fprintf ( outf, "#include \"%s\"\n", cfg->tileset_file );
-#if 0
-  
+  //
+  // we write one layer at a time
+  //
+  map_t* map = term->map;
+  //
+  // structure layer: this one is handled using fixed objects
+  // and additional logic to guess the orientation of objects
+  // such as open doors, corners, etc.
   for ( int y1 = 1 ; ( y1 < 22 ) ; ++y1 ) {
-        for ( int x1 = 0 ; x1 < curr_map->ncols ; ++x1 ) {
-	  // current info on this glyph
-	  glyph_t* g1      = 0; // map_get(curr_map,x1,y1);
-	  double hx = curr_map->hero_x;
-	  double hy = curr_map->hero_y;
-	  uint16_t flags = g1->flags;
-	  char     chr   = g1->ascii;
-	  char     color = g1->color;
-	  int is_hero = (x1 == hx) && (y1 == hy);
-	  if (chr == ' ') { // nothing here
-		  continue;
-	  }
-	  if (chr != '>') { // if ladder down, don't put a floor
- 	    fprintf( outf, "object { Floor ");
-	    fprintf( outf, " translate < %2d, %2d,  0 > }\n", x1, y1);
-	  }
-	  // ceiling
-	  if (chr != '<') { // if ladder up, don't put a ceiling
- 	    fprintf( outf, "object { Ceiling ");
-	    fprintf( outf, " translate < %2d, %2d,  0 > }\n", x1, y1);
-	  }
-	  // thing	 
-	  fprintf ( outf, "object { Tiles[%d][%d]\n", chr, color);	  
-	  if (is_hero) { fprintf(outf," no_shadow\n"); }
-	  if (!is_monster(chr)) {
-	    // monsters don't move
-	    fprintf( outf, " translate < %2d, %2d,  0>\n}\n", x1, y1);
-	  }  else {
-	    //
-	    // monsters can move
-	    //
-	    const double dx1 = g1->dx;
-	    const double dy1 = g1->dy;
-	    
-	    // previous info on this glyph
-	    // is located at its previous position (given by displacement)
-	    const int x0 = x1 - g1->dx;
-	    const int y0 = y1 - g1->dy;
-	    const glyph_t* g0 = 0; // map_get(prev_map,x0,y0);
-	    double dx0, dy0;
-	    double dx, dy;
-	    double angle;
-	    double x, y;
-	    if (g0->code != g1->code) {
-	      // not same glyph!
-	      printf("not same glyph!\n");
-	      dx0 = dx1;
-	      dy0 = dy1;
-	      x = (1.0-T)*x0 + T*x1;
-	      y = (1.0-T)*y0 + T*y1;	    
-	      dx = (1.0-T)*dx0 + T*dx1;
-	      dy = (1.0-T)*dy0 + T*dy1;	    
-	      angle = 0; // 90 - (180.0/M_PI)*atan2f(dy, dx);
+    for ( int x1 = 0 ; x1 < map->ncols ; ++x1 ) {
+      glyph_t* g = map_get_dungeon(map,x1,y1);
+      switch(g->ascii) {
+      case '.':
+	put_floor(x1,y1,outf);
+	break;
+      case '|':
+	put_wall(x1,y1,90,outf);
+	break;
+      case '-':
+	if (y1 > 0) {
+	  glyph_t* up = map_get_dungeon(map,x1,y1-1);
+	  if (up->ascii == '|') {
+	    // some this is a corner
+	    if (x1 > 0) {
+	      glyph_t* left = map_get_dungeon(map,x1-1,y1);
+	      if (left->ascii == '-') {
+		// southeast corner
+		put_corner(x1,y1,-90,outf);
+	      } else {
+	        // southwest corner
+		put_corner(x1,y1,0,outf);
+	      }
 	    } else {
-	      dx0 = g0->dx;
-	      dy0 = g0->dy;
-	      x = (1.0-T)*x0 + T*x1;
-	      y = (1.0-T)*y0 + T*y1;	    
-	      dx = (1.0-T)*dx0 + T*dx1;
-	      dy = (1.0-T)*dy0 + T*dy1;	    
-	      angle = (180.0/M_PI)*atan2f(dy, dx) - 90;
+	        // southwest corner
+		put_corner(x1,y1,0,outf);
+	      // southwest corner
 	    }
-	    //printf("glyph %c %03d: T=%6.3f curr=(%d,%d) prev=(%2d,%2d) dcurr=(%6.3f,%6.3f) dprev=(%6.3f,%6.3f) dinst=(%6.3f,%6.3f) angle=%7.2f\n",g1->ascii, g1->code,
-	    //	   T,x1,y1,x0,y0,dx0,dy0,dx1,dy1,dx,dy,angle);
-	    // interpolated speed is only for computing the rotation angle
-	    fprintf( outf, " rotate %5f*z\n", angle );
-	    // when T < 1, we are showing the maps between the previous and
-	    // the current. Thus we need to displace the glyph *backwards* 
-	    // according to current speed and position
-	    fprintf( outf, " translate < %7.3f, %7.3f,  0>\n}\n", x, y);
-	    if (is_hero) { // is this our hero??
-	      fprintf ( outf, "light_source { GlobalLight translate < %7.3f, %7.3f,  0 > }\n", x, y );
-	      fprintf ( outf, "light_source { LocalLight  translate < %7.3f, %7.3f,  0 > }\n", x, y);
-	      fprintf ( outf, "camera {\n" );
-	      fprintf ( outf, "  perspective\n" );
-	      fprintf ( outf, "  right (1920/1080)*x\n" );
-	      fprintf ( outf, "  sky z\n" );
-	      fprintf ( outf, "  location < %7.3f, %7.3f, CameraHeight> \n", x, y+5);
-	      fprintf ( outf, "  look_at  < %7.3f, %7.3f, 0.5>\n", x, y );
-	      fprintf ( outf, "}\n" );
-	    }
+	  } else {
+	    // horizontal wall
+	    put_wall(x1,y1,0,outf);
 	  }
-        }
+	} else if (y1 < (map->nrows-1)) {
+	  glyph_t* down = map_get_dungeon(map,x1,y1+1);
+	  if (down->ascii == '|') {
+	    // this is a corner
+	    if (x1 > 0) {
+	      glyph_t* left = map_get_dungeon(map,x1-1,y1);
+	      if (left->ascii == '-') {
+	        // northeast corner
+		put_corner(x1,y1,180,outf);
+	      } else {
+	        // northwest corner
+		put_corner(x1,y1,90,outf);
+	      }
+	    } else {
+	      // northwest corner
+	      put_corner(x1,y1,90,outf);
+	    }
+	  } else {
+	    // horizontal wall
+	    put_wall(x1,y1,90,outf);
+	  }
+	}
+	break;
+      case '+':
+	{
+	  glyph_t dummy;
+	  dummy.ascii = 0;
+	  dummy.code  = 0;
+	  // check surrounding walls for orientation
+	  glyph_t* n = y1 > 0 ? map_get_dungeon(map,x1,y1-1) : &dummy;
+	  glyph_t* s = y1 < (map->nrows-1) ? map_get_dungeon(map,x1,y1+1) : &dummy;
+	  glyph_t* w = x1 > 0 ? map_get_dungeon(map,x1-1,y1) : &dummy;
+	  glyph_t* e = x1 < (map->ncols-1) ? map_get_dungeon(map,x1+1,y1) : &dummy;
+	  if ((n->ascii == '|') || (s->ascii == '|')) {
+	    // vertical door for sure
+	    put_door(x1,y1,90,outf);
+	  } else if ((e->ascii == '-') || (w->ascii == '-')) {
+	    // horizontal door
+	    put_door(x1,y1,0,outf);
+	  } else {
+	    // unhandled door orientation?	    
+	    put_door(x1,y1,45,outf);
+	  }
+	}
+	break;
+      case '#':
+	put_floor(x1,y1,outf);
+	break;
+      case '<':
+	put_ladder_up(x1,y1,outf);
+	break;
+      case '>':
+	put_ladder_down(x1,y1,outf);
+	break;
+      case '_':
+	put_altar(x1,y1,outf);
+	break;
+      case '}':
+	put_fountain(x1,y1,outf);
+	break;
+      case '{':
+	put_water(x1,y1,outf);
+	break;
+      case '^':
+	put_trap(x1,y1,outf);
+	break;
+      default:
+	break;
+      }
     }
-#endif
+  }
+  //
+  // objects: the easiest
+  //
+  for ( int y1 = 1 ; ( y1 < 22 ) ; ++y1 ) {
+    for ( int x1 = 0 ; x1 < map->ncols ; ++x1 ) {
+      glyph_t* g = map_get_object(map,x1,y1);
+      fprintf ( outf, "object { Tiles[%d][%d] ", g->ascii, g->color);	  
+      fprintf( outf, " translate < %2d, %2d,  0>\n}\n", x1, y1);
+    }
+  }
+  //
+  // monsters: these move
+  //
+  double hx = map->hero_x;
+  double hy = map->hero_y;
+  for ( int y1 = 1 ; ( y1 < 22 ) ; ++y1 ) {
+    for ( int x1 = 0 ; x1 < map->ncols ; ++x1 ) {
+      int is_hero = (x1 == hx) && (y1 == hy);
+      glyph_t* g1 = map_get_monster(map,x1,y1);
+      const double dx1 = g1->dx;
+      const double dy1 = g1->dy;
+      
+      // previous info on this glyph
+      // is located at its previous position (given by displacement)
+      const int x0 = x1 - g1->dx;
+      const int y0 = y1 - g1->dy;
+      const glyph_t* g0 = map_get_monster(map,x0,y0);
+      double dx0, dy0;
+      double dx, dy;
+      double angle;
+      double x, y;
+      const double T = time;
+      if (g0->code != g1->code) {
+	// not same glyph!
+	printf("not same glyph!\n");
+	dx0 = dx1;
+	dy0 = dy1;
+	x = (1.0-T)*x0 + T*x1;
+	y = (1.0-T)*y0 + T*y1;	    
+	dx = (1.0-T)*dx0 + T*dx1;
+	dy = (1.0-T)*dy0 + T*dy1;	    
+	angle = 0; // 90 - (180.0/M_PI)*atan2f(dy, dx);
+      } else {
+	dx0 = g0->dx;
+	dy0 = g0->dy;
+	x = (1.0-T)*x0 + T*x1;
+	y = (1.0-T)*y0 + T*y1;	    
+	dx = (1.0-T)*dx0 + T*dx1;
+	dy = (1.0-T)*dy0 + T*dy1;	    
+	angle = (180.0/M_PI)*atan2f(dy, dx) - 90;
+      }
+      fprintf ( outf, "object { Tiles[%d][%d] ", g1->ascii, g1->color);	  
+      fprintf( outf, " rotate %5f*z\n", angle );
+      // when T < 1, we are showing the maps between the previous and
+      // the current. Thus we need to displace the glyph *backwards* 
+      // according to current speed and position
+      fprintf( outf, " translate < %7.3f, %7.3f,  0>\n}\n", x, y);
+      if (is_hero) { // is this our hero??
+	fprintf ( outf, "light_source { GlobalLight translate < %7.3f, %7.3f,  0 > }\n", x, y );
+	fprintf ( outf, "light_source { LocalLight  translate < %7.3f, %7.3f,  0 > }\n", x, y);
+	fprintf ( outf, "camera {\n" );
+	fprintf ( outf, "  perspective\n" );
+	fprintf ( outf, "  right (1920/1080)*x\n" );
+	fprintf ( outf, "  sky z\n" );
+	fprintf ( outf, "  location < %7.3f, %7.3f, CameraHeight> \n", x, y+5);
+	fprintf ( outf, "  look_at  < %7.3f, %7.3f, 0.5>\n", x, y );
+	fprintf ( outf, "}\n" );
+      }      
+    } // for x
+  } // for y
+  //
+  // we cannot identify effects  for now
+  //
+
 }
